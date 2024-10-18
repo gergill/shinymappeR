77576a1a69277409c1e8d7d30a771da532a7ebf8
George CK <georgeck612@gmail.com>
2024-10-18 10:33:38 -0500

    initial commit

---
+++ app.R
@@ -0,0 +1,58 @@
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    https://shiny.posit.co/
+#
+
+library(shiny)
+library(devtools)
+install_github("https://github.com/Uiowa-Applied-Topology/mappeR/tree/dev")
+library(mappeR)
+
+# Define UI for application that draws a histogram
+ui <- fluidPage(
+
+    # Application title
+    titlePanel("1D mapper on a circle"),
+
+    # Sidebar with a slider input for number of bins
+    sidebarLayout(
+        sidebarPanel(
+            sliderInput("bins",
+                        "Number of bins:",
+                        min = 1,
+                        max = 50,
+                        value = 30),
+            sliderInput("percent_overlap",
+                        "percent overlap:",
+                        min = 0,
+                        max = 100,
+                        value = 25),
+        ),
+
+        # Show a plot of the generated distribution
+        mainPanel(
+           plotOutput("circle"),
+           plotOutput("distPlot")
+        )
+    )
+)
+
+# Define server logic required to draw a histogram
+server <- function(input, output) {
+    data = data.frame(x = sapply(1:1000, cos) + runif(1000, 0, .1), y = sapply(1:1000, sin) + runif(1000, 0, .1))
+    dists = dist(data)
+
+    output$circle <- renderPlot({
+        plot(data, pch=20, asp=1)
+    })
+    output$distPlot <- renderPlot({
+        plot(mapper_object_to_igraph(create_1D_mapper_object(data, dists, data$x, create_width_balanced_cover(min(data$x), max(data$x), input$bins, input$percent_overlap))))
+    })
+}
+
+# Run the application
+shinyApp(ui = ui, server = server)
