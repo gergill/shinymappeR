01563551acc970bdb550f351b0ec660d3ac26016
georgeck612 <georgeck612@gmail.com>
2024-12-04 12:15:12 -0600

    add commenting and cleanup

--- app.R
+++ app.R
@@ -9,28 +9,56 @@
 
 library(shiny)
 library(devtools)
-install_github("https://github.com/Uiowa-Applied-Topology/mappeR/tree/dev")
 library(mappeR)
 
-# Define UI for application that draws a histogram
-ui <- fluidPage(# Application title
+# stolen from sir jacob miller
+generate_spiral <- function(n=1000, noise=0.1){
+  t <- runif(n, 0, 4 * pi)
+  h <- runif(n, 0, 1)
+
+  hole_mask <- (t > 1.5*pi & t < 1.75*pi) & (h>0.25 & h<0.75)
+  t <- t[!hole_mask]
+  h <- h[!hole_mask]
+
+  x <- t*cos(t) + rnorm(length(t), 0, noise)
+  z <- t*sin(t) + rnorm(length(t), 0, noise)
+
+  return(data.frame(x=x, y=z))
+}
+
+generate_barbell <- function(n) {
+  r1 = sqrt(runif(n*.45, 0, .5))
+  a1 = runif(n*.45, 0, 2*pi)
+  disk1 = data.frame(x=r1*cos(a1) - 1, y=r1*sin(a1))
+
+  r2 = sqrt(runif(n*.45, 0, .5))
+  a2 = runif(n*.45, 0, 2*pi)
+  disk2 = data.frame(x=r2*cos(a2) + 1, y=r2*sin(a2))
+
+  line = data.frame(x = runif(n/20, -.5, .5), y = rep(0, n/20))
+
+  return(rbind(disk1, disk2, line))
+}
+
+# Define UI for application that constructs mapper graph
+ui <- fluidPage(
     titlePanel("1D Mapper"),
 
-    # Sidebar with a slider input for number of bins
+    # Sidebar with parameter input options
     sidebarLayout(
         sidebarPanel(
-            selectInput(
-                "data",
-                "Dataset",
-                choices = c("circle", "figure 8", "spiral", "barbell")
+            selectInput( # this is a drop down list
+                "data", # internal variable name
+                "Dataset", # display name
+                choices = c("circle", "figure 8", "spiral", "barbell") # choices for drop down
             ),
-            sliderInput(
-                "points",
-                "Number of points",
-                value = 1000,
-                min = 100,
-                max = 2000,
-                step = 100
+            sliderInput( # this is a slider
+                "points", # internal variable name
+                "Number of points", # display name
+                value = 1000, # initial value
+                min = 100, # min value
+                max = 2000, # max value
+                step = 100 # step size for slider bar
             ),
             selectInput(
                 "projection",
@@ -59,40 +87,13 @@ ui <- fluidPage(# Application title
             selectInput("global", "Global or local clustering?", choices = c("global", "local"))
         ),
 
-        # Show a plot of the generated distribution
-        mainPanel(plotOutput("circle"), plotOutput("distPlot"))
+        # plot mapper graph
+        mainPanel(plotOutput("inputdata"), plotOutput("mapper"))
     ))
 
-# Define server logic required to draw a histogram
+# Define server logic required to construct mapper graph
 server <- function(input, output) {
-
-    generate_swiss <- function(n_samples=1000, noise=0.1){
-        t <- runif(n_samples, 0, 4 * pi)
-        h <- runif(n_samples, 0, 1)
-
-        #create hole
-
-        hole_mask <- (t > 1.5*pi & t < 1.75*pi) & (h>0.25 & h<0.75)
-        t <- t[!hole_mask]
-        h <- h[!hole_mask]
-
-        x <- t*cos(t) + rnorm(length(t), 0, noise)
-        z <- t*sin(t) + rnorm(length(t), 0, noise)
-
-        return(data.frame(x=x, y=z))
-
-    }
-
-    generate_barbell <- function(n) {
-        r1 = sqrt(runif(n*.45, 0, .5))
-        a1 = runif(n*.45, 0, 2*pi)
-        disk1 = data.frame(x=r1*cos(a1) - 1, y=r1*sin(a1))
-        r2 = sqrt(runif(n*.45, 0, .5))
-        a2 = runif(n*.45, 0, 2*pi)
-        disk2 = data.frame(x=r2*cos(a2) + 1, y=r2*sin(a2))
-        line = data.frame(x = runif(n/20, -.5, .5), y = rep(0, n/20))
-        return(rbind(disk1, disk2, line))
-    }
+    # generate sample data
     data = reactive({
         switch(
             input$data,
@@ -104,13 +105,13 @@ server <- function(input, output) {
                 x = sapply(1:input$points, function(x) cos(x) / (1 + sin(x)^2)) + runif(input$points, 0, .1),
                 y = sapply(1:input$points, function(x) sin(x)*cos(x) / (1 + sin(x)^2)) + runif(input$points, 0, .1)
             ),
-            "spiral" = generate_swiss(input$points),
+            "spiral" = generate_spiral(input$points),
             "barbell" = generate_barbell(input$points)
         )
     })
 
+    # generate cover
     cover = reactive({
-        data = data()
         projection = switch(input$projection, "x" = data$x, "y" = data$y)
         create_width_balanced_cover(min(projection),
                                     max(projection),
@@ -118,6 +119,7 @@ server <- function(input, output) {
                                     input$percent_overlap)
     })
 
+    # generate mapper graph
     mapper = reactive({
         data = data()
         cover = cover()
@@ -134,7 +136,7 @@ server <- function(input, output) {
         )
     })
 
-    output$circle <- renderPlot({
+    output$inputdata <- renderPlot({
         data = data()
         projection = switch(input$projection, "x" = data$x, "y" = data$y)
         cover = cover()
