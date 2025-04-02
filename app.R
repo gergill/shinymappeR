b3b4d4767803fe9be61022cfb3deeaaacca93e7e
George Clare Kennedy <georgeck612@gmail.com>
2025-04-02 08:36:41 -0500

    add level set viz

--- app.R
+++ app.R
@@ -13,12 +13,23 @@ source("dataset_generation.R")
 source("lens_functions.R")
 source("cover_logic.R")
 
-color_gradient = colorRampPalette(c('blue', 'gold', 'red'))
+color_gradient <- function(n, alpha=0) {
+  colors <- colorRampPalette(c('blue', 'gold', 'red'))(n)
+  if (alpha == 0) {
+    return(colors)
+  } else {
+    return(paste(colors, sprintf("%x", ceiling(255 * alpha)), sep = ""))
+  }
+}
 
 # Define UI for application that constructs mapper graph
 ui <- fluidPage(
 
+# title panel -------------------------------------------------------------
   titlePanel("1D Mapper"),
+
+
+# dataset input panel  ----------------------------------------------------
 
   # Sidebar with parameter input options
   sidebarLayout(
@@ -75,6 +86,9 @@ ui <- fluidPage(
     mainPanel(plotOutput("inputdata"))
   ),
 
+
+# lens input panel --------------------------------------------------------
+
   sidebarLayout(
     sidebarPanel(
       selectInput(
@@ -90,6 +104,39 @@ ui <- fluidPage(
       )
     ),
     mainPanel(plotOutput("filtered_data"))
+  ),
+
+
+# cover input panel -------------------------------------------------------
+
+  sidebarLayout(
+    sidebarPanel(
+      sliderInput(
+        inputId = "num_patches",
+        label = "Number of Patches: ",
+        value = 10,
+        min = 1,
+        max = 20,
+        step = 1
+      ),
+      sliderInput(
+        inputId = "percent_overlap",
+        label = "Percent Overlap: ",
+        value = 15,
+        min = 0,
+        max = 100,
+        step = 1
+      ),
+      sliderInput(
+        inputId = "display_patch",
+        label = "Patch to Display: ",
+        value = 1,
+        min = 1,
+        max = 2,
+        step = 1
+      )
+    ),
+    mainPanel(plotOutput("patchwork_cover"))
   )
 
 )
@@ -125,6 +172,11 @@ server <- function(input, output) {
     )
   })
 
+  # when water change, update air
+  observeEvent(input$num_patches,  {
+    updateSliderInput(inputId = "display_patch", value = input$display_patch, min = 1, max = input$num_patches)
+  })
+
   # generate cover
   cover = reactive({
     # grab current data
@@ -136,7 +188,7 @@ server <- function(input, output) {
     # create 1D width-balanced cover
     create_width_balanced_cover(min(filtered_data),
                                 max(filtered_data),
-                                input$bins,
+                                input$num_patches,
                                 input$percent_overlap)
   })
 
@@ -156,7 +208,7 @@ server <- function(input, output) {
                             dist(data),
                             filtered_data,
                             cover,
-                            clusterer = hierarchical_clusterer(input$method))
+                            clusterer = hierarchical_clusterer("single"))
   })
 
 
@@ -182,6 +234,23 @@ server <- function(input, output) {
 
     # plot data with appropriate coloring
     plot(data, pch = 20, axes = FALSE, xlab="", ylab="", col = col, asp=1)
+  })
+
+  output$patchwork_cover <- renderPlot({
+    data = data()
+    filtered_data = filtered_data()
+    cover = cover()
+    mapper = mapper()
+
+    vertices = mapper[[1]]
+
+    this_patch = vertices[vertices$bin == input$display_patch, ]
+    this_patch_data = this_patch[, "data"]
+    this_patch_names = unlist(strsplit(this_patch_data, ","))
+    rows = as.numeric(this_patch_names)
+    datasub = data[rows,]
+    plot(datasub, pch=20, axes=FALSE, xlab = "", ylab = "", asp = 1)
+    rect(min(datasub$x), min(datasub$y), max(datasub$x), max(datasub$y), col = color_gradient(input$num_patches, .5)[[input$display_patch]])
   })
 
   # output plot of mapper graph
