70318415101718e13daaf5724c7eb84eb2f4ee2c
George <georgeck612@gmail.com>
2025-05-05 22:08:28 -0500

    add lens functions to new viz edition

--- lens_functions.R
+++ lens_functions.R
@@ -1,6 +1,15 @@
-pca_filter <- function(data, n) {
+filter_data <- function(input, data) {
+  switch(input,
+         "eccentricity" = eccentricity(data),
+         "PCA-1" = pca_filter(data),
+         "project to x" = data$x,
+         "project to y" = data$y,
+         data$x)
+}
+
+pca_filter <- function(data) {
   pca_output = prcomp(data, center = FALSE, scale. = FALSE)
-  return(pca_output$x[,n])
+  return(pca_output$x[,1])
 }
 
 eccentricity <- function(data) {
--- shinymapper2.R
+++ shinymapper2.R
@@ -1,13 +1,10 @@
+library(shiny)
+library(devtools)
+library(mappeR)
+library(bslib)
+library(ggplot2)
 source("dataset_generation.R")
 source("lens_functions.R")
-
-cards <- list(
-  card(
-    full_screen = TRUE,
-    card_header("Data"),
-    plotOutput("data")
-  )
-)
 
 data_inputs <- sidebar(
   title = "Data Parameters",
@@ -23,22 +20,42 @@ data_inputs <- sidebar(
   sliderInput(
     "noise", "Noise",
     0, 1, .1
+  ),
+  selectInput(
+    "lens", "Lens",
+    c("project to x", "project to y", "eccentricity", "PCA-1"),
+    selected = "project to x"
   )
 )
 
-ui <- page_sidebar(
-  title = "Dataset Generation",
-  sidebar = data_inputs,
-  !!!cards
+cards <- list(
+  card(
+    full_screen = TRUE,
+    card_header("Data"),
+    layout_sidebar(
+      sidebar = data_inputs,
+      plotOutput("data")
+    )
+  )
+)
+
+ui <- page_navbar(
+  title = "One Dimensional Mapper",
+  nav_panel("Dataset Generation", cards[[1]])
 )
 
 server <- function(input, output) {
+  data <- reactive({generate_data(input$dataset, input$num_points, input$noise)})
+  filtered_data <- reactive({filter_data(input$lens, data())})
   gg_plot <- reactive({
-    ggplot(generate_data(input$dataset, input$num_points, input$noise), aes(x=x, y=y)) +
+    data = data()
+    filtered_data = filtered_data()
+    ggplot(data, aes(x=x, y=y, color = filtered_data)) +
       theme_minimal() +
       theme(axis.title = element_blank()) +
       geom_point() +
-      coord_fixed()
+      coord_fixed() +
+      scale_color_viridis_c(option = "plasma", name=input$lens)
   })
 
   output$data <- renderPlot(gg_plot())
