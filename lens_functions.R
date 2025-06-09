9987b33ae9a821a82b30bd776d4cb1f790d760b8
gergill <georgeck612@gmail.com>
2025-06-09 01:26:49 -0500

    remove junk code

--- lens_functions.R
+++ lens_functions.R
@@ -1,18 +1,3 @@
-filter_data <- function(input, data) {
-  switch(input,
-         "eccentricity" = eccentricity(data),
-         "PCA-1" = pca_filter(data),
-         "project to x" = data$x,
-         "project to y" = data$y,
-         data$x)
-}
-
-pca_filter <- function(data) {
-  pca_output = prcomp(data, center = FALSE, scale. = FALSE)
-  print(pca_output)
-  return(pca_output$x[,1])
-}
-
 eccentricity <- function(data) {
   dists = as.matrix(dist(data))
   return(apply(dists, 1, sum))
