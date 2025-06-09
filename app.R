7376b0606c46431f346b447a8743a624c17fced2
gergill <georgeck612@gmail.com>
2025-06-08 23:27:05 -0500

    rearrange everything, remove labels from dendrograms

--- app.R
+++ app.R
@@ -25,6 +25,7 @@ color_gradient <- function(n, alpha = 0) {
   }
 }
 
+
 # Define UI for application that constructs mapper graph
 ui <- navbarPage(
   "1D Mapper",
@@ -33,342 +34,357 @@ ui <- navbarPage(
 # classic flavor ----------------------------------------------------------
 
 
-  tabPanel("Overview",
-           sidebarLayout(
-             sidebarPanel(
-               selectInput( # this is a drop down list
-                 "data", # internal variable name
-                 "Dataset", # display name
-                 choices = c("circle", "figure 8", "spiral", "barbell") # choices for drop down
-               ),
-               sliderInput( # this is a slider
-                 "points", # internal variable name
-                 "Number of points", # display name
-                 value = 1000, # initial value
-                 min = 100, # min value
-                 max = 2000, # max value
-                 step = 100 # step size for slider bar
-               ),
-               sliderInput(
-                 inputId = "noise",
-                 label = "Noise",
-                 value = .1,
-                 min = 0,
-                 max = 1,
-                 step = 0.01
-               ),
-               selectInput(
-                 "lens",
-                 "Lens Function: ",
-                 choices = c("project to x", "project to y", "use eccentricity value", "PCA-1", "PCA-2")
-               ),
-               sliderInput(
-                 "num_patches",
-                 "Number of patches:",
-                 min = 1,
-                 max = 20,
-                 value = 10
-               ),
-               sliderInput(
-                 "percent_overlap",
-                 "Percent overlap:",
-                 min = 0,
-                 max = 100,
-                 value = 25
-               ),
-               selectInput(
-                 "method",
-                 "Linkage method",
-                 choices = c("single", "complete", "average", "ward.D2", "mcquitty")
-               ),
-	       selectInput(
-		"clusterer",
-		"Cutting Height Method",
-		choices = c("global", "local")
-	       )
-
-             ),
-
-             # plot mapper graph
-             mainPanel(plotOutput("staggered_data"), plotOutput("mapper"))
-           )
-  ),
-
-
   # lens input panel --------------------------------------------------------
 
-  tabPanel(
-    "Patch Dendrograms",
-    sidebarLayout(sidebarPanel(
-      selectInput(
-        "lens_seg",
-        "Lens Function: ",
-        choices = c(
-          "project to x",
-          "project to y",
-          "use eccentricity value",
-          "PCA-1",
-          "PCA-2"
-        )
-      ),
-      sliderInput(
-        inputId = "display_patch",
-        label = "Patch to Display: ",
-        value = 1,
-        min = 1,
-        max = 2,
-        step = 1
-      )
-    ),
-    mainPanel(plotOutput("patch_view"), plotOutput("patch_dendrogram"), plotOutput("global_dendrogram")))
-  )
-)
+	tabPanel("Data, Lenses, and Covering",
+		sidebarLayout(
+			sidebarPanel(
+			
+				selectInput(
+					"data",
+					"Dataset",
+					choices = c("circle", "figure 8", "spiral", "barbell")
+					),
+			
+				sliderInput( # this is a slider
+					"points", # internal variable name
+					"Number of points", # display name
+					value = 1000, # initial value
+					min = 100, # min value
+					max = 2000, # max value
+					step = 100 # step size for slider bar
+					),
+
+				sliderInput(
+					inputId = "noise",
+					label = "Noise",
+					value = .1,
+					min = 0,
+					max = 1,
+					step = 0.01
+					),
+
+				selectInput(
+					"lens",
+					"Lens Function: ",
+					choices = c(
+					  "project to x",
+					  "project to y",
+					  "use eccentricity value",
+					  "PCA-1",
+					  "PCA-2"
+					  )
+					),
+
+				sliderInput(
+					"num_patches",
+					"Number of patches:",
+					min = 1,
+					max = 20,
+					value = 10
+					),
+				sliderInput(
+						"percent_overlap",
+						"Percent overlap:",
+						min = 0,
+						max = 100,
+						value = 25
+						)
+					),
+					mainPanel(
+						plotOutput("staggered_data"), 
+						plotOutput("mapper")
+					)
+				)
+			),
+
+			tabPanel("Hierarchical Clustering",
+				 sidebarLayout(
+				sidebarPanel(
+					sliderInput(
+						inputId = "display_patch",
+						label = "Patch to Display: ",
+						value = 1,
+						min = 1,
+					max = 2,
+					step = 1
+			      ),
+
+				selectInput(
+					"method",
+					"Linkage method",
+					choices = c("single", "complete", "average", "mcquitty")
+			      ),
+
+				selectInput(
+					"clusterer",
+					"Cutting Height Method",
+					choices = c("global", "local")
+			      )
+			)
+				,
+				mainPanel(
+					plotOutput("patch_view"),
+					plotOutput("dendrograms")
+				)
+			 )
+		)
+	)
 
 
-# Define server logic required to construct mapper graph
-server <- function(input, output) {
+	# Define server logic required to construct mapper graph
+	server <- function(input, output) {
 
 
-  # when lens changes, update selection
-  observeEvent(input$lens, {
-    updateSelectInput(
-      inputId = "lens_seg",
-      selected = input$lens
-    )
-  })
+	  # when lens changes, update selection
+	  observeEvent(input$lens, {
+	    updateSelectInput(
+	      inputId = "lens",
+	      selected = input$lens
+	    )
+	  })
 
-  # when lens changes, update selection
-  observeEvent(input$lens_seg, {
-    updateSelectInput(
-      inputId = "lens",
-      selected = input$lens_seg
-    )
-  })
-
-  # when patch total changes, update selection
-  observeEvent(input$num_patches, {
-    updateSliderInput(
-      inputId = "display_patch",
-      max = input$num_patches
-    )
-  })
+	  # when patch total changes, update selection
+	  observeEvent(input$num_patches, {
+	    updateSliderInput(
+	      inputId = "display_patch",
+	      max = input$num_patches
+	    )
+	  })
 
 
-  # data generation and mapper steps ----------------------------------------
+	  # data generation and mapper steps ----------------------------------------
 
-  # generate sample data
-  data = reactive({
-    switch(
-      input$data,
-      "circle" = generate_circle(input$points, input$noise),
-      "figure 8" = generate_figure_eight(input$points, input$noise),
-      "spiral" = generate_spiral(input$points, input$noise),
-      "barbell" = generate_barbell(input$points, input$noise)
-    )
-  })
+	  # generate sample data
+	  data = reactive({
+	    switch(
+	      input$data,
+	      "circle" = generate_circle(input$points, input$noise),
+	      "figure 8" = generate_figure_eight(input$points, input$noise),
+	      "spiral" = generate_spiral(input$points, input$noise),
+	      "barbell" = generate_barbell(input$points, input$noise)
+	    )
+	  })
 
-  # filter data
-  filtered_data = reactive({
-    # grab current data
-    data = data()
+	  # filter data
+	  filtered_data = reactive({
+	    # grab current data
+	    data = data()
 
-    switch(
-      input$lens,
-      "project to x" = data$x,
-      "project to y" = data$y,
-      "use eccentricity value" = eccentricity(data),
-      "PCA-1" = pca_filter(data, 1),
-      "PCA-2" = pca_filter(data, 2)
-    )
-  })
+	    switch(
+	      input$lens,
+	      "project to x" = data$x,
+	      "project to y" = data$y,
+	      "use eccentricity value" = eccentricity(data),
+	      "PCA-1" = pca_filter(data, 1),
+	      "PCA-2" = pca_filter(data, 2)
+	    )
+	  })
 
-  # generate cover
-  cover = reactive({
-    # grab current data
-    data = data()
+	  # generate cover
+	  cover = reactive({
+	    # grab current data
+	    data = data()
 
-    # grab current filter values
-    filtered_data = filtered_data()
+	    # grab current filter values
+	    filtered_data = filtered_data()
 
-    # create 1D width-balanced cover
-    create_width_balanced_cover(
-      min(filtered_data),
-      max(filtered_data),
-      input$num_patches,
-      input$percent_overlap
-    )
-  })
+	    # create 1D width-balanced cover
+	    create_width_balanced_cover(
+	      min(filtered_data),
+	      max(filtered_data),
+	      input$num_patches,
+	      input$percent_overlap
+	    )
+	  })
 
-  # select global/local clusterer
-  clusterer = reactive({
-    data = data()
-    dists = dist(data)
-    switch(
-	   input$clusterer,
-	   "local" = local_tallest_hierarchical_clusterer(input$method),
-	   "global" = global_tallest_hierarchical_clusterer(input$method, dists)
-    )
-  })
+	  # select global/local clusterer
+	  clusterer = reactive({
+	    data = data()
+	    dists = dist(data)
+	    switch(
+		   input$clusterer,
+		   "local" = local_tallest_hierarchical_clusterer(input$method),
+		   "global" = global_tallest_hierarchical_clusterer(input$method, dists)
+	    )
+	  })
 
-  # generate mapper graph
-  mapper = reactive({
-    # grab current data
-    data = data()
+	  # generate mapper graph
+	  mapper = reactive({
+	    # grab current data
+	    data = data()
 
-    # grab current filter values
-    filtered_data = filtered_data()
+	    # grab current filter values
+	    filtered_data = filtered_data()
 
-    # grab current cover
-    cover = cover()
+	    # grab current cover
+	    cover = cover()
 
-    # grab current clusterer
-    clusterer = clusterer()
+	    # grab current clusterer
+	    clusterer = clusterer()
 
-    # create mapper graph
-    create_1D_mapper_object(data,
-                            dist(data),
-                            filtered_data,
-			    cover,
-                            clusterer)
-  })
+	    # create mapper graph
+	    create_1D_mapper_object(data,
+				    dist(data),
+				    filtered_data,
+				    cover,
+				    clusterer)
+	  })
 
 
-  # output plots ------------------------------------------------------------
+	  # output plots ------------------------------------------------------------
 
-  # output plot of data
-  output$inputdata <- renderPlot({
-    # grab data
-    data = data()
+	  # output plot of data
+	  output$inputdata <- renderPlot({
+	    # grab data
+	    data = data()
 
-    # plot data
-    plot(
-      data,
-      pch = 20,
-      axes = FALSE,
-      xlab = "",
-      ylab = "",
-      asp = 1
-    )
-  })
+	    # plot data
+	    plot(
+	      data,
+	      pch = 20,
+	      axes = FALSE,
+	      xlab = "",
+	      ylab = "",
+	      asp = 1
+	    )
+	  })
 
-  # output plot of filtered data
-  output$filtered_data <- renderPlot({
-    # grab data and filtered data
-    data = data()
-    filtered_data = filtered_data()
+	  # output plot of filtered data
+	  output$filtered_data <- renderPlot({
+	    # grab data and filtered data
+	    data = data()
+	    filtered_data = filtered_data()
 
-    # create a vector of colors according to lens function
-    col <- color_gradient(50)[as.numeric(cut(filtered_data, breaks = 50))]
+	    # create a vector of colors according to lens function
+	    col <- color_gradient(50)[as.numeric(cut(filtered_data, breaks = 50))]
 
-    # plot data with appropriate coloring
-    plot(
-      data,
-      pch = 20,
-      axes = FALSE,
-      xlab = "",
-      ylab = "",
-      col = col,
-      asp = 1
-    )
-  })
+	    # plot data with appropriate coloring
+	    plot(
+	      data,
+	      pch = 20,
+	      axes = FALSE,
+	      xlab = "",
+	      ylab = "",
+	      col = col,
+	      asp = 1
+	    )
+	  })
 
-  output$patch_view <- renderPlot({
-    data = data()
-    filtered_data = filtered_data()
-    cover = cover()
-    mapper = mapper()
+	  output$patch_view <- renderPlot({
+	    data = data()
+	    global_dists = dist(data)
+	    filtered_data = filtered_data()
+	    cover = cover()
+	    mapper = mapper()
 
-    vertices = mapper[[1]]
+	    vertices = mapper[[1]]
 
-    print(vertices$bin)
 
-    this_patch = vertices[vertices$bin == input$display_patch, ]
-    this_patch_data = this_patch[, "data"]
-    this_patch_names = unlist(strsplit(this_patch_data, ","))
-    rows = as.numeric(this_patch_names)
-    datasub = data[rows, ]
+	    this_patch = vertices[vertices$bin == input$display_patch, ]
+	    this_patch_data = this_patch[, "data"]
+	    this_patch_names = unlist(strsplit(this_patch_data, ","))
+	    rows = as.numeric(this_patch_names)
+	    datasub = data[rows, ]
+	    patch_dists = dist(datasub)
 
-    plot(
-      datasub,
-      pch = 20,
-      axes = FALSE,
-      xlab = "",
-      ylab = "",
-      asp = 1
-    )
+	    par(mfrow = c(1, 2))
 
-    rect(
-      min(datasub$x),
-      min(datasub$y),
-      max(datasub$x),
-      max(datasub$y),
-      col = color_gradient(input$num_patches, .5)[[input$display_patch]]
-    )
-  })
+	    plot(
+	      datasub,
+	      pch = 20,
+	      axes = FALSE,
+	      xlab = "",
+	      ylab = "",
+	      asp = 1
+	    )
 
-  output$patch_dendrogram <- renderPlot({
-    data = data()
-    filtered_data = filtered_data()
-    cover = cover()
-    mapper = mapper()
+	    rect(
+	      min(datasub$x),
+	      min(datasub$y),
+	      max(datasub$x),
+	      max(datasub$y),
+	      col = color_gradient(input$num_patches, .5)[[input$display_patch]]
+	    )
 
-    vertices = mapper[[1]]
 
-    this_patch = vertices[vertices$bin == input$display_patch, ]
-    this_patch_data = this_patch[, "data"]
-    this_patch_names = unlist(strsplit(this_patch_data, ","))
-    rows = as.numeric(this_patch_names)
-    datasub = data[rows, ]
-    patch_dists = dist(datasub)
-    global_dists = dist(data)
+	    patch_dend = hclust(patch_dists, input$method)
+	    global_dend = hclust(global_dists, input$method)
 
-    patch_dend = hclust(patch_dists, input$method)
-    global_dend = hclust(global_dists, input$method)
+	    global_cut_height = get_tallest_branch_height(global_dend, max(global_dists))
 
-    if (input$clusterer == "global") {
-      cut_height = get_tallest_branch_height(global_dend, max(global_dists))
+	    patch_cut_height = global_cut_height
 
-    plot_dendrogram(patch_dend, input$method, max(global_dists), cut_height, paste("Patch", input$display_patch, ":", input$method, "linkage clustering"))
-    } else {
-      cut_height = get_tallest_branch_height(patch_dend, max(patch_dists))
-    plot_dendrogram(patch_dend, input$method, max(patch_dists), cut_height, paste("Patch", input$display_patch, ":", input$method, "linkage clustering"))
-    }
-  })
+	    if (input$clusterer == "local") {
+	      patch_cut_height = get_tallest_branch_height(patch_dend, max(patch_dists))
 
-  # output plot of mapper graph
-  output$mapper <- renderPlot({
-    # plot igraph object obtained from mappeR
-    plot(mapper_object_to_igraph(mapper()))
-  })
+	    plot_dendrogram(patch_dend, input$method, max(patch_dists), patch_cut_height, paste("Patch", input$display_patch), paste("Linkage:", input$method))
+	    } else {
 
-  output$global_dendrogram <- renderPlot({
-    data = data()
-    dists = dist(data)
-    dend = hclust(dists, input$method)
+	    plot_dendrogram(patch_dend, input$method, max(global_dists), patch_cut_height, paste("Patch", input$display_patch), paste("Linkage:", input$method))
+	    }
+	  })
 
-    cut_height = get_tallest_branch_height(dend, max(dists))
+	  output$dendrograms <- renderPlot({
+	    data = data()
+	    filtered_data = filtered_data()
+	    cover = cover()
+	    mapper = mapper()
 
-    plot_dendrogram(dend, input$method, max(dists), cut_height, paste("All data:", input$method, "linkage clustering"))
+	    vertices = mapper[[1]]
 
-    # plot(plot_dendrogram(dend, input$method, 2, max(dists)))
-  })
+	    this_patch = vertices[vertices$bin == input$display_patch, ]
+	    this_patch_data = this_patch[, "data"]
+	    this_patch_names = unlist(strsplit(this_patch_data, ","))
+	    rows = as.numeric(this_patch_names)
+	    datasub = data[rows, ]
+	    patch_dists = dist(datasub)
+	    global_dists = dist(data)
 
-  # plot of "staggered" level sets for x/y projection
-  output$staggered_data <- renderPlot({
-    data = data()
-    filtered_data = filtered_data()
-    cover = cover()
 
-    # plot data
-    plot(data, pch = 20)
+	    global_dend = hclust(global_dists, input$method)
+	    global_cut_height = get_tallest_branch_height(global_dend, max(global_dists))
 
-    if (input$lens == "project to x") {
-      rect(cover[, 1], min(data$y), cover[, 2], max(data$y), col = color_gradient(input$num_patches, .5))
-    } else if (input$lens == "project to y") {
-      rect(min(data$x), cover[, 2], max(data$x), cover[, 1], col = color_gradient(input$num_patches, .5))
-    }
-  })
-}
+	    par(mfrow = c(1, 2))
 
-# Run the application
-shinyApp(ui = ui, server = server)
+	    # create a vector of colors according to lens function
+	    col <- color_gradient(50)[as.numeric(cut(filtered_data, breaks = 50))]
+
+	    # plot data with appropriate coloring
+	    plot(
+	      data,
+	      pch = 20,
+	      axes = FALSE,
+	      xlab = "",
+	      ylab = "",
+	      col = col,
+	      asp = 1
+	    )
+	    plot_dendrogram(global_dend, input$method, max(global_dists), global_cut_height, "All Data", paste("Linkage:", input$method))
+	  })
+
+	  # output plot of mapper graph
+	  output$mapper <- renderPlot({
+	    # plot igraph object obtained from mappeR
+	    plot(mapper_to_igraph(mapper()))
+	  })
+
+	  # plot of "staggered" level sets for x/y projection
+	  output$staggered_data <- renderPlot({
+	    data = data()
+	    filtered_data = filtered_data()
+	    cover = cover()
+
+	    # plot data
+	    plot(data, pch = 20)
+
+	    if (input$lens == "project to x") {
+	      rect(cover[, 1], min(data$y), cover[, 2], max(data$y), col = color_gradient(input$num_patches, .5))
+	    } else if (input$lens == "project to y") {
+	      rect(min(data$x), cover[, 2], max(data$x), cover[, 1], col = color_gradient(input$num_patches, .5))
+	    }
+	  })
+	}
+
+	# Run the application
+	shinyApp(ui = ui, server = server)
