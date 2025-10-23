12a691104d29777a110b29e96e69e979e7eb8818
Jacob Miller <Jamiller219@gmail.com>
2025-10-23 13:36:20 -0500

    flake: improve shell hook, formatters, add just and latex

--- flake.nix
+++ flake.nix
@@ -1,24 +1,55 @@
 {
-  description = "A basic flake with a shell";
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-  inputs.systems.url = "github:nix-systems/default";
-  inputs.flake-utils = {
-    url = "github:numtide/flake-utils";
-    inputs.systems.follows = "systems";
+  description = "ShinyMappeR flake for usage and development.";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
   };
 
-  outputs =
-    { nixpkgs, flake-utils, ... }:
-    flake-utils.lib.eachDefaultSystem (
-      system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
-      in
-      {
-        devShells.default = pkgs.mkShell { 
-	packages = [ pkgs.bashInteractive ];
-	buildInputs = with pkgs; [ R rPackages.mappeR rPackages.shiny rPackages.dendextend rPackages.RColorBrewer rPackages.devtools rPackages.ggplot2 ];
-	};
-      }
-    );
+  outputs = { self, nixpkgs, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+
+      rEnv = pkgs.rWrapper.override {
+        packages = with pkgs.rPackages; [
+          ggplot2
+          shiny
+          dplyr
+          tidyr
+          devtools
+          remotes
+          RColorBrewer
+          mclust
+        ];
+      };
+
+      texlive = pkgs.texlive.combine {
+        inherit (pkgs.texlive) scheme-small collection-latexextra;
+      };
+    in {
+      formatter.${system} = pkgs.nixfmt-rfc-style;
+
+      devShells.${system}.default = pkgs.mkShell {
+        LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
+          pkgs.stdenv.cc.cc
+          pkgs.libz
+        ];
+
+        LC_ALL = "en_US.UTF-8";
+
+        buildInputs = with pkgs; [
+          rEnv
+          texlive
+          just
+          self.formatter.${system}
+        ];
+
+        shellHook = ''
+          echo "Entering R dev environment..."
+          echo "R: $(R --version | head -n 1)"
+          echo "LaTeX: $(which pdflatex)"
+          just --list-heading $'Commands:\n' --list-prefix "    just " --no-aliases --list
+        '';
+      };
+    };
 }
