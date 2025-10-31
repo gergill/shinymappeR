9688d4c4a89728e2094ab896e1f2fe93f0e23a6c
Jacob Miller <Jamiller219@gmail.com>
2025-10-30 22:58:51 -0500

    dev: move visualization functions to separate module

--- app.R
+++ app.R
@@ -14,7 +14,7 @@ source("lens_functions.R")
 source("hierarchical_clusterers.R")
 source("plot_dendrograms.R")
 source("gmapper_cover.R")
-
+source("visualization.R")
 # ------------------------------------------------------------------
 # USER INTERFACE
 # ------------------------------------------------------------------
@@ -84,10 +84,14 @@ ui <- navbarPage(
         ),
         hr(),
         sliderInput("display_patch", "Patch to display:", value = 1, min = 1, max = 2),
-        selectInput("method", "Linkage method:",
+        selectInput(
+          "method",
+          "Linkage method:",
           choices = c("single", "complete", "average", "mcquitty")
         ),
-        selectInput("clusterer", "Cutting height method:",
+        selectInput(
+          "clusterer",
+          "Cutting height method:",
           choices = c("global", "local")
         )
       ),
@@ -106,6 +110,7 @@ ui <- navbarPage(
 # ------------------------------------------------------------------
 
 server <- function(input, output, session) {
+
   # --- Dynamic Dataset Parameter UI -----------------------------------------
   observeEvent(input$data, {
     generator <- get_generator(input$data)
@@ -122,7 +127,7 @@ server <- function(input, output, session) {
     })
   })
 
-  # --- Data Reactive --------------------------------------------------------
+  # --- Reactive: Data -------------------------------------------------------
   data <- reactive({
     if (!is.null(input$upload)) {
       df <- tryCatch(
@@ -153,20 +158,18 @@ server <- function(input, output, session) {
     }
   })
 
-  # --- Filtered Data ---------------------------------------------------------
+  # --- Reactive: Filtered (Lens) Data --------------------------------------
   filtered_data <- reactive({
     df <- data()
     lens_obj <- get_lens(input$lens)
     params <- lapply(names(lens_obj$param_spec), function(p) input[[p]])
     names(params) <- names(lens_obj$param_spec)
-
     vals <- lens_obj$compute(df, params)
-    # 🟢 Ensure names align with the dataset
-    names(vals) <- rownames(df)
+    names(vals) <- rownames(df)  # ensure alignment
     vals
   })
 
-  # --- Cover Creation --------------------------------------------------------
+  # --- Reactive: Cover ------------------------------------------------------
   cover <- reactive({
     lens <- filtered_data()
     if (input$cover_method == "Width-Balanced") {
@@ -185,7 +188,7 @@ server <- function(input, output, session) {
     }
   })
 
-  # --- Mapper Object ---------------------------------------------------------
+  # --- Reactive: Clusterer --------------------------------------------------
   clusterer <- reactive({
     dists <- dist(data())
     switch(input$clusterer,
@@ -194,6 +197,7 @@ server <- function(input, output, session) {
     )
   })
 
+  # --- Reactive: Mapper Object ---------------------------------------------
   mapper <- reactive({
     create_1D_mapper_object(
       data(),
@@ -204,10 +208,9 @@ server <- function(input, output, session) {
     )
   })
 
-  # --- update the "Patch to display" slider when cover changes ----
+  # --- Reactive: Update Display Patch Slider -------------------------------
   observeEvent(list(input$cover_method, input$num_patches, cover()), {
     if (input$cover_method == "Width-Balanced") {
-      # For width-balanced covers, patches = num_patches
       updateSliderInput(
         session,
         "display_patch",
@@ -227,183 +230,42 @@ server <- function(input, output, session) {
     }
   })
 
-  # --- Plots -----------------------------------------------------------------
+  # ------------------------------------------------------------------
+  # VISUALIZATIONS 
+  # ------------------------------------------------------------------
 
   output$filtered_data <- renderPlot({
-    vals <- filtered_data()
-    df <- data()
-    col <- colorRampPalette(brewer.pal(9, "YlOrRd"))(50)
-    col_assign <- col[as.numeric(cut(vals, breaks = 50))]
-    plot(df, col = col_assign, pch = 20, asp = 1, axes = FALSE)
+    plot_filtered_data(data(), filtered_data())
   })
 
   output$mapper <- renderPlot({
-    plot(mapper_to_igraph(mapper()))
+    plot_mapper_graph(mapper())
   })
 
   output$staggered_data <- renderPlot({
-    df <- data()
-    cov <- cover()
-    lens_obj <- get_lens(input$lens)
-
-    # Base scatter plot
-    plot(df,
-      asp = 1, pch = 20, col = "grey",
-      xlab = "", ylab = "",
-      xlim = range(df$x), ylim = range(df$y)
+    plot_staggered_data(
+      data(),
+      cover(),
+      get_lens(input$lens),
+      input,
+      filtered_data()
     )
-
-    if (lens_obj$projection) {
-      n_cov <- nrow(cov)
-      cov_colors <- viridis::viridis(n_cov, alpha = 0.35, option = "D")
-
-      if (input$lens == "project to x") {
-        # ---- horizontal rectangles spanning y ----
-        for (i in seq_len(n_cov)) {
-          rect(
-            xleft = cov[i, 1],
-            ybottom = min(df$y),
-            xright = cov[i, 2],
-            ytop = max(df$y),
-            col = cov_colors[i],
-            border = NA
-          )
-        }
-      } else if (input$lens == "project to y") {
-        # ---- vertical rectangles spanning x ----
-        for (i in seq_len(n_cov)) {
-          rect(
-            xleft = min(df$x),
-            ybottom = cov[i, 1],
-            xright = max(df$x),
-            ytop = cov[i, 2],
-            col = cov_colors[i],
-            border = NA
-          )
-        }
-      } else if (input$lens == "theta lens") {
-        pinfo <- lens_obj$projection_fn(df, input$theta)
-        pvec <- pinfo$vector / sqrt(sum(pinfo$vector^2))
-
-        # Perpendicular direction = circle radius
-        perp <- c(-pvec[2], pvec[1])
-        perp <- perp / sqrt(sum(perp^2))
-
-
-        # Draw cover strips perpendicular to radius (i.e. along tangent)
-        for (i in seq_len(n_cov)) {
-          # Compute endpoints along tangent direction
-          cut1 <- pinfo$point + cov[i, 1] * pvec
-          cut2 <- pinfo$point + cov[i, 2] * pvec
-
-          # Expand in radial perpendicular direction for visible strip width
-          c1 <- cut1 + 100 * perp
-          c2 <- cut1 - 100 * perp
-          c3 <- cut2 - 100 * perp
-          c4 <- cut2 + 100 * perp
-
-          polygon(
-            x = c(c1[1], c2[1], c3[1], c4[1]),
-            y = c(c1[2], c2[2], c3[2], c4[2]),
-            col = cov_colors[i], border = NA
-          )
-        }
-      } else if (grepl("PCA", input$lens)) {
-        # ---- PCA projection lines/cover polygons ----
-        pinfo <- lens_obj$projection_fn(df)
-        pvec <- pinfo$vector / sqrt(sum(pinfo$vector^2))
-        slope <- pvec[2] / pvec[1]
-
-        # Draw main PCA direction
-        abline(a = 0, b = slope, col = "darkgreen", lwd = 3, lty = 3)
-
-        # Draw cover tiles perpendicular to PCA vector
-        perp <- c(-pvec[2], pvec[1])
-        perp <- perp / sqrt(sum(perp^2))
-
-        for (i in seq_len(n_cov)) {
-          cut1 <- cov[i, 1] * pvec
-          cut2 <- cov[i, 2] * pvec
-
-          c1 <- cut1 + 100 * perp
-          c2 <- cut1 - 100 * perp
-          c3 <- cut2 - 100 * perp
-          c4 <- cut2 + 100 * perp
-
-          polygon(
-            x = c(c1[1], c2[1], c3[1], c4[1]),
-            y = c(c1[2], c2[2], c3[2], c4[2]),
-            col = cov_colors[i], border = NA
-          )
-        }
-      }
-    } else {
-      # Color points by non-projection lens value
-      vals <- filtered_data()
-      val_colors <- colorRampPalette(brewer.pal(9, "YlOrRd"))(50)
-      col_assign <- val_colors[as.numeric(cut(vals, breaks = 50))]
-      points(df, col = col_assign, pch = 20)
-    }
   })
 
   output$patch_view <- renderPlot({
-    df <- data()
-    mapper_obj <- mapper()
-    vertices <- mapper_obj[[1]]
-    global_dists <- dist(df)
-
-    this_patch <- vertices[vertices$patch == input$display_patch, ]
-    this_patch_data <- this_patch[, "data"]
-    this_patch_names <- unlist(strsplit(this_patch_data, ","))
-    rows <- as.numeric(this_patch_names)
-    datasub <- df[rows, ]
-
-    patch_dists <- dist(datasub)
-    patch_dend <- hclust(patch_dists, input$method)
-    global_dend <- hclust(global_dists, input$method)
-    global_cut <- get_longevity_cut_height(global_dend, max(global_dists))
-    patch_cut <- if (input$clusterer == "local") {
-      get_longevity_cut_height(patch_dend, max(patch_dists))
-    } else {
-      global_cut
-    }
-
-    par(mfrow = c(1, 2))
-    clusters <- cutree(patch_dend, h = patch_cut)
-    num_clusts <- length(unique(clusters))
-    cols <- brewer.pal(num_clusts, "Dark2")
-    col_assign <- sapply(clusters, function(x) cols[x])
-    plot(datasub, pch = 20, col = col_assign, asp = 1, axes = FALSE)
-    plot_dendrogram(
-      patch_dend, input$method,
-      if (input$clusterer == "local") max(patch_dists) else max(global_dists),
-      patch_cut, paste("Patch", input$display_patch),
-      paste("Linkage:", input$method)
+    plot_patch_view(
+      data(),
+      mapper(),
+      input$display_patch,
+      input$method,
+      input$clusterer
     )
   })
 
   output$global_view <- renderPlot({
-    df <- data()
-    global_dists <- dist(df)
-    global_dend <- hclust(global_dists, input$method)
-    global_cut <- get_longevity_cut_height(global_dend, max(global_dists))
-
-    par(mfrow = c(1, 2))
-    clusters <- cutree(global_dend, h = global_cut)
-    num_clusts <- length(unique(clusters))
-    cols <- brewer.pal(num_clusts, "Dark2")
-    col_assign <- sapply(clusters, function(x) cols[x])
-
-    plot(df, pch = 20, col = col_assign, asp = 1, axes = FALSE)
-    plot_dendrogram(
-      global_dend, input$method,
-      max(global_dists), global_cut,
-      "All Data", paste("Linkage:", input$method)
-    )
+    plot_global_view(data(), input$method)
   })
 }
 
-# ------------------------------------------------------------------
-# RUN APP
-# ------------------------------------------------------------------
+# RUN
 shinyApp(ui = ui, server = server)
---
+++ visualization.R
@@ -0,0 +1,151 @@
+library(viridis)
+library(RColorBrewer)
+
+# --- Filtered Data Plot ------------------------------------
+plot_filtered_data <- function(df, vals) {
+  col <- colorRampPalette(brewer.pal(9, "YlOrRd"))(50)
+  col_assign <- col[as.numeric(cut(vals, breaks = 50))]
+  plot(df, col = col_assign, pch = 20, asp = 1, axes = FALSE)
+}
+
+# --- Mapper Graph Visualization ----------------------------
+plot_mapper_graph <- function(mapper_obj) {
+  plot(mapper_to_igraph(mapper_obj))
+}
+
+# --- Staggered Data + Cover Visualization ------------------
+plot_staggered_data <- function(df, cov, lens_obj, input, filtered_vals) {
+  plot(
+    df,
+    asp = 1, pch = 20, col = "grey",
+    xlab = "", ylab = "",
+    xlim = range(df$x), ylim = range(df$y)
+  )
+
+  if (lens_obj$projection) {
+    n_cov <- nrow(cov)
+    cov_colors <- viridis::viridis(n_cov, alpha = 0.35, option = "D")
+
+    if (input$lens == "project to x") {
+      for (i in seq_len(n_cov)) {
+        lty_style <- ifelse(i %% 2 == 0, 2, 1)
+        rect(
+          cov[i, 1], min(df$y), cov[i, 2], max(df$y),
+          col = cov_colors[i],
+          border = "black",
+          lty = lty_style
+        )
+      }
+
+    } else if (input$lens == "project to y") {
+      for (i in seq_len(n_cov)) {
+        lty_style <- ifelse(i %% 2 == 0, 2, 1)
+        rect(
+          min(df$x), cov[i, 1], max(df$x), cov[i, 2],
+          col = cov_colors[i],
+          border = "black",
+          lty = lty_style
+        )
+      }
+
+    } else if (input$lens == "theta lens") {
+      pinfo <- lens_obj$projection_fn(df, input$theta)
+      pvec <- pinfo$vector / sqrt(sum(pinfo$vector^2))
+      perp <- c(-pvec[2], pvec[1]) / sqrt(sum(pvec^2))
+
+      for (i in seq_len(n_cov)) {
+        lty_style <- ifelse(i %% 2 == 0, 2, 1)
+        cut1 <- pinfo$point + cov[i, 1] * pvec
+        cut2 <- pinfo$point + cov[i, 2] * pvec
+        rect_coords <- rbind(
+          cut1 + 100 * perp,
+          cut1 - 100 * perp,
+          cut2 - 100 * perp,
+          cut2 + 100 * perp
+        )
+        polygon(
+          rect_coords[, 1], rect_coords[, 2],
+          col = cov_colors[i],
+          border = "black",
+          lty = lty_style
+        )
+      }
+
+    } else if (grepl("PCA", input$lens)) {
+      pinfo <- lens_obj$projection_fn(df)
+      pvec <- pinfo$vector / sqrt(sum(pinfo$vector^2))
+      slope <- pvec[2] / pvec[1]
+      abline(a = 0, b = slope, col = "darkgreen", lwd = 3, lty = 3)
+      perp <- c(-pvec[2], pvec[1]) / sqrt(sum(pvec^2))
+
+      for (i in seq_len(n_cov)) {
+        lty_style <- ifelse(i %% 2 == 0, 2, 1)
+        cut1 <- cov[i, 1] * pvec
+        cut2 <- cov[i, 2] * pvec
+        rect_coords <- rbind(
+          cut1 + 100 * perp,
+          cut1 - 100 * perp,
+          cut2 - 100 * perp,
+          cut2 + 100 * perp
+        )
+        polygon(
+          rect_coords[, 1], rect_coords[, 2],
+          col = cov_colors[i],
+          border = "black",
+          lty = lty_style
+        )
+      }
+    }
+  } else {
+    val_colors <- colorRampPalette(brewer.pal(9, "YlOrRd"))(50)
+    col_assign <- val_colors[as.numeric(cut(filtered_vals, breaks = 50))]
+    points(df, col = col_assign, pch = 20)
+  }
+}
+
+# --- Patch View --------------------------------------------
+plot_patch_view <- function(df, mapper_obj, display_patch, method, clusterer_mode) {
+  vertices <- mapper_obj[[1]]
+  global_dists <- dist(df)
+  this_patch <- vertices[vertices$patch == display_patch, ]
+  rows <- as.numeric(unlist(strsplit(this_patch$data, ",")))
+  datasub <- df[rows, ]
+
+  patch_dists <- dist(datasub)
+  patch_dend <- hclust(patch_dists, method)
+  global_dend <- hclust(global_dists, method)
+  global_cut <- get_longevity_cut_height(global_dend, max(global_dists))
+  patch_cut <- if (clusterer_mode == "local") {
+    get_longevity_cut_height(patch_dend, max(patch_dists))
+  } else {
+    global_cut
+  }
+
+  par(mfrow = c(1, 2))
+  clusters <- cutree(patch_dend, h = patch_cut)
+  cols <- brewer.pal(length(unique(clusters)), "Dark2")
+  plot(datasub, pch = 20, col = cols[clusters], asp = 1, axes = FALSE)
+  plot_dendrogram(
+    patch_dend, method,
+    if (clusterer_mode == "local") max(patch_dists) else max(global_dists),
+    patch_cut, paste("Patch", display_patch),
+    paste("Linkage:", method)
+  )
+}
+
+# --- Global View -------------------------------------------
+plot_global_view <- function(df, method) {
+  global_dists <- dist(df)
+  global_dend <- hclust(global_dists, method)
+  global_cut <- get_longevity_cut_height(global_dend, max(global_dists))
+
+  par(mfrow = c(1, 2))
+  clusters <- cutree(global_dend, h = global_cut)
+  cols <- brewer.pal(length(unique(clusters)), "Dark2")
+  plot(df, pch = 20, col = cols[clusters], asp = 1, axes = FALSE)
+  plot_dendrogram(
+    global_dend, method,
+    max(global_dists), global_cut,
+    "All Data", paste("Linkage:", method)
+  )
+}
